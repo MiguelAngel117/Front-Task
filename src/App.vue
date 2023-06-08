b04d416886589eb1a8449ab55e5e780010b41561
MiguelAngel117 <miguel.choconta@uptc.edu.co>
2023-06-08 17:16:45 -0500

    Componentes

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <TareaItem />
+</template>
+
+<script>
+import TareaItem from './components/Task'
+export default {
+  name: 'App',
+  components: {
+    TareaItem
+  }
+}
+</script>
+
+<style>
+
+</style>
