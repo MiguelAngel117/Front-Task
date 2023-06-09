5b9719bbc26800a941b3a4a86de9c4ce808ebe12
MiguelAngel117 <miguel.choconta@uptc.edu.co>
2023-06-08 19:08:23 -0500

    dist

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -16,14 +16,16 @@
                         </div>
                         <br>
                         {{listaTareas}}
+                        <h5 v-if="listaTareas.length == 0">NO HAY TAREAS PARA REALIZAR</h5>
                             <ul class="list-group">
                                 <li v-for="(tarea, index) of listaTareas" :key="index" 
                                 class="list-group-item d-flex justify-content-between">
-                                    <span class="cursor">
-                                    <i class="far fa-circle"></i>
+                                    <span class="cursor" v-bind:class="{'text-success': tarea.estado}"
+                                    v-on:click="editarTarea(tarea, index)">
+                                    <i v-bind:class="[tarea.estado ?'fas fa-check-circle':'far fa-circle']"></i>
                                     </span>
                                     {{tarea.nombre}}
-                                    <span class="text-danger cursor">
+                                    <span class="text-danger cursor" v-on:click="eliminarTarea(index)">
                                         <i class="fas fa-trash"></i>
                                     </span>
                                 </li>
@@ -52,6 +54,12 @@
                 }
                 this.listaTareas.push(tarea);
                 this.tarea = '';
+            },
+            eliminarTarea(index){
+                this.listaTareas.splice(index,1);
+            },
+            editarTarea(tarea, index){
+                this.listaTareas[index].estado = !tarea.estado;
             }
         }
     }
